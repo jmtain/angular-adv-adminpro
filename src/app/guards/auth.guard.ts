52e2220d1a530f918bbe8f1571ee74dea9a66ff3
Jose Manuel Tain Vilanova <tain@tain.es>
2021-03-29 19:32:15 +0200

    Lista para producción

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanLoad, Route, UrlSegment } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UsuarioService } from '../services/usuario.service';
 import { tap } from 'rxjs/operators';
@@ -7,9 +7,19 @@ import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private usuarioService: UsuarioService, private router: Router) {    
+  }
+  canLoad(route: Route, segments: UrlSegment[]): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    return this.usuarioService.validarToken()
+    .pipe(
+      tap (estaAutenticado => {
+        if (!estaAutenticado) {
+          this.router.navigateByUrl('/login');
+        }
+      })
+    );
   }
   canActivate(
     route: ActivatedRouteSnapshot,
